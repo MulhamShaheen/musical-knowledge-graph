18fabb386e226e870c0a82ce80f906bbad5785c8
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2025-01-15 01:44:28 +0300

    add triplets generation for embedder

--- generate_graph.py
+++ generate_graph.py
@@ -14,7 +14,7 @@ ontology = Ontology(
 generator = GraphGenerator(ontology, "music:")
 generator.add_individual("song1", [("by", "artist1"), ("inAlbum", "album1"), ("inGenre", "genre1")], "Song")
 
-# generator.load_dataset("data/yandex.csv", "song")
-#
-# generator.serialize("music.owl", format="xml")
-generator.load_graph_from_file("music.owl", format="xml")
+generator.load_dataset("data/yandex.csv", "song")
+generator.serialize("music1.owl", format="xml")
+generator.load_graph_from_file("music1.owl", format="xml")
+generator.save_triplets("triplets.csv")
